7dac1b4499e8c32b576648ba1f17ae270faccb02
Ashwin Srinath <ashwin@barre2099.clemson.edu>
2017-08-16 18:33:49 -0400

    Add data generator

---
+++ makeData.py
@@ -0,0 +1,35 @@
+import os
+import numpy as np
+import pandas as pd
+from glob import glob
+
+def create_weather(growth=12800):
+    filenames = sorted(glob(os.path.join('data', 'weather-small', '*.hdf5')))
+
+    if not os.path.exists(os.path.join('data', 'weather-big')):
+        os.mkdir(os.path.join('data', 'weather-big'))
+
+    #if all(os.path.exists(fn.replace('small', 'big')) for fn in filenames):
+    #    return
+
+    from scipy.misc import imresize
+    import h5py
+
+    for fn in filenames:
+        print("Creating weather data from file {}".format(fn))
+        with h5py.File(fn, mode='r') as f:
+            x = f['/t2m'][:]
+
+        y = np.float64(imresize(x, growth))
+ 
+        out_fn = os.path.join('data', 'weather-big', os.path.split(fn)[-1])
+
+        try:
+            with h5py.File(out_fn) as f:
+                f.create_dataset('/t2m', data=y, chunks=(500, 500))
+        except:
+            pass
+
+
+if __name__ == '__main__':
+    create_weather()
