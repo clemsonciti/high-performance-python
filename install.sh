d36db1a0b75109993d7161872285eaa6639b47db
Ashwin Srinath <atrikut@clemson.edu>
2017-08-17 09:08:44 -0400

    Add scikit image

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@
 module load osg-proxy
 conda create -n hpp-workshop python=3.5
 source activate hpp-workshop
-conda install -y numpy pandas h5py Pillow matplotlib scipy toolz pytables snakeviz dask distributed numba ipykernel
+conda install -y numpy pandas h5py Pillow matplotlib scipy toolz pytables snakeviz dask distributed numba ipykernel scikit-image
+
 python -m ipykernel install --user --name hpp-workshop --display-name "High Performance Python workshop"
